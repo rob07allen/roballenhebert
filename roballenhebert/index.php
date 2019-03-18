1e821a3012c94b48679ca45084db8058f228e16b
rob07allen <Rob07allen@gmail.com>
2019-03-18 12:52:52 -0500

    Add files via upload

---
+++ roballenhebert/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<head>
+	<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo ('template_directory'); ?>/style.css"> <!--This is the code for linking a CSS file with the HTML-->
+	</head>
+<title>Rob Allen Hebert</title>
+<h1>Rob Allen Hebert</h1>
+<image src="https://roballenhebert.files.wordpress.com/2019/03/20901750_10154690295666646_4113858495759055121_o-4.jpg" alt="avatar">
+<
+<body> <p>Growing as a Web Developer!</p></body>
+
+
+
+
